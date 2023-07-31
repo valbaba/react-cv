6dd4b06bef8acf5c12262d4a64f911f2bd98f0a4
Valentin BRIEZ-BANULS <valentin.briez-banuls@imerir.com>
2023-08-01 01:43:38 +0200

    ADD - New way to add App and call ParallaxView

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ParallaxView from "./views/Parallax";
+
+
+function App() {
+
+    return (
+        <div className='App'>
+            <ParallaxView/>
+        </div>
+
+    );
+}
+
+export default App;
